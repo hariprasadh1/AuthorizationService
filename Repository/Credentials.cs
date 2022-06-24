613abab524e083b4d52808b663cc30be83986474
hariprasadh1 <hariprasadht2000@gmail.com>
2022-06-24 13:27:44 +0530

    Add project files.

---
+++ Repository/Credentials.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationService.Repository
+{
+    public class Credentials: ICredentials
+    {
+        private Dictionary<string,string> ValidUsersDictionary = new Dictionary<string, string>()
+        {
+               {"1","Hariprasadh"},
+               {"2","Charulatha"},
+               {"3","Anjali"},
+               {"4","Aishwarya"}
+        };
+        public Dictionary<string,string> GetCredentials()
+        {
+            return ValidUsersDictionary;
+        }
+    }
+}
